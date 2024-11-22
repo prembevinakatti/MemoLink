fdc9ec2f08de2039d7a165088455cfa0b9f48c9b
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-11-22 19:27:12 +0530

    ..

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY =
+  "pk_test_c3RpcnJpbmctaGFtc3Rlci04LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
+  </StrictMode>
+);
