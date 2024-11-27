d299b6e0e4f7961828f768945eb9318fead7be22
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-11-27 20:08:21 +0530

    ..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const PUBLISHABLE_KEY =
-  "pk_test_c3RpcnJpbmctaGFtc3Rlci04LmNsZXJrLmFjY291bnRzLmRldiQ";
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
-    </ClerkProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
